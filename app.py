8b3a99864b56bed1835462b59464b2bd6e025c9b
Elsa <elsashakiba@gmail.com>
2024-06-23 20:45:41 +0430

    Add Flask-based weather forecast web application
    - Initialize Flask project and set up basic structure
    - Implement route for handling GET and POST requests
    - Integrate OpenWeatherMap API for fetching weather data
    - Create HTML template for user input and displaying weather information
    - Add error handling for API requests and user input

--- app.py
+++ app.py
@@ -1,32 +1,39 @@
 import requests
+from flask import Flask ,request,render_template
+app = Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+def index ():
+    if request.method == 'POST':
+        city = request.form['city']
+        api_key = '00edc2ccf69244eaf25aa870ba0db7af'
+        base_url = 'https://api.openweathermap.org/data/2.5/weather?'
+        params = {'q': city, 'appid': api_key, 'units': 'metric'}
+        try:
+            response = requests.get(base_url, params=params)
+            data = response.json()
 
-def get_weather(city):
-    api_key = '00edc2ccf69244eaf25aa870ba0db7af' 
-    base_url = 'https://api.openweathermap.org/data/2.5/weather?'
-    params = {'q': city, 'appid': api_key, 'units': 'metric'}
+            if data["cod"] == 200:
+               weather_description = data['weather'][0]['description']
+               temp = data['main']['temp']
+               humidity = data['main']['humidity']
+               wind_speed = data['wind']['speed'] 
 
-    try:
-        response = requests.get(base_url, params=params)
-        data = response.json()
-
-        if data["cod"] == 200:
-            weather_description = data['weather'][0]['description']
-            temp = data['main']['temp']
-            humidity = data['main']['humidity']
-            wind_speed = data['wind']['speed']
-
-            print(f"Weather in {city}:")
-            print(f"Description: {weather_description}")
-            print(f"Temperature: {temp}°C")
-            print(f"Humidity: {humidity}%")
-            print(f"Wind Speed: {wind_speed} m/s")
-        else:
-            print(f"Error: {data['message']}")
-
-    except Exception as e:
-        print(f"Error fetching data: {e}")
-
-if __name__ == "__main__":
-    city = input("Enter city name: ")
-    get_weather(city)
+               weather_info={
+                   'description':weather_description,
+                   'temperature':temp,
+                   'humidity':humidity,
+                   'wind_speed':wind_speed,
+                   'city':city
+               }    
+               return render_template ('index.html',weather=weather_info)
+            else:
+                error_message=data['message']
+                return render_template('index.html',error=error_message)
+        except Exception as e:
+            error_message = str(e)
+            return render_template ('index.html',error=error_message)
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
+              
 
